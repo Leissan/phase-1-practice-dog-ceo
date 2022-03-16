14b1a55bb983c4eba7501f1cdb879bbe1ea75b1c
Leissan Khan <leissan.khan@gmail.com>
2022-03-16 19:14:40 -0400

    commit tree 46a71f769e217ed6ee9b0f87db141b3137c22c91

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+const imgList=JSON.stringify(imgUrl)
+
+console.log(imgList)
+
+//Add JavaScript that:
+//on page load, fetches the images using the url above ⬆️
+//parses the response as JSON
+//adds image elements to the DOM for each 🤔 image in the array
+
+function addImage(){
+    let card=document.createElement('li')
+    card.className='card'
+    card.innerHTML=`
+    <img src="${message}" img width="300">
+
+    document.querySelector("#dog-image-container").appendChild(card)`
+}
+
+
+function challengeOne(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res=> res.json())
+    .then(data => {
+        data.message.forEach(imageUrl=>{
+                const img=document.createElement('img')
+                img.src=imageUrl
+                document.getElementById("dog-image-container").append(img)
+        })
+
+    })
+}
+
+challengeOne()
+
